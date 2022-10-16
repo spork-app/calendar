c93dfad6b5c1a1b931b73d583902ed5e0881dfd9
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 20:34:35 -0400

    Add basic test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spork\Calendar\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use Spork\Calendar\CalendarServiceProvider.php;
+
+class TestCase extends TestbenchTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Spork\\Calendar\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            CalendarServiceProvider.php::class,
+        ];
+    }
+}
+
